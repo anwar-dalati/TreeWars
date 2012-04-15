c9a732895d174f3e35a2702d9e0e118f249c0c94
Patrick Schmidt <batterie1@gmx.net>
2012-04-15 02:57:44 +0200

    remove module.exports from frontend file

--- public/javascripts/ImageManager.js
+++ public/javascripts/ImageManager.js
@@ -1,30 +1,30 @@
 var ImageManager = function()
 {
 	var that = this;
-	
+
 	/*
 	 * preloaded images
 	 */
-	var images = [];	
-	
+	var images = [];
+
 	var backgroundImage = null;
-	
+
 	this.preloadAllImages = function()
 	{
-		
+
 	}
-	
-	
+
+
 	this.preloadImage =  function(imageUrl, identifier, position, width, height)
 	{
 		//images[identifier] = new Image();
-		images[identifier].src = "" + imageUrl;	
+		images[identifier].src = "" + imageUrl;
 		images[identifier].positions = position;
 		images[identifier].width = width;
 		images[identifier].height = height;
 		images[identifier].id = identifier;
-	} 
-	
+	}
+
 	this.preloadBackgroundImage = function(imageUrl, width, height, identifier)
 	{
 		backgroundImage.src = "" + imageUrl;
@@ -32,27 +32,27 @@ var ImageManager = function()
 		backgroundImage.height = height;
 		backgroundImage.id = identifier;
 	}
-	
+
 	this.accessImage = function(identifier)
 	{
 		return images[identifier];
 	}
-	
+
 	this.getImagePositions = function(identifier)
 	{
 		return images[identifier].positions;
 	}
-	
+
 	this.setImagePositions = function(identifier, positions)
 	{
 		images[identifier].positions = positions;
 	}
-	
+
 	this.addImagePosition = function(identifier, position)
 	{
 		images[identifier].positions[positions.length] = position;
 	}
-	
+
 	this.removeImagePosition = function(identifier, position)
 	{
 		for (positionIndex = 0; positionIndex < images[i].positions.length; positionIndex = positionIndex + 1)
@@ -62,15 +62,15 @@ var ImageManager = function()
 				if(images[i].positions[positionIndex].y)
 				{
 					images[i].positions.splice(positionIndex, 1);
-					break;	
-				}	
-			}				
+					break;
+				}
+			}
 		}
-	}	
-	
-	
+	}
+
+
 	this.drawImages = function()
-	{		
+	{
 		$(document).ready(function()
 		{
 			$('#'+ backgroundImage.id).animate(
@@ -78,24 +78,19 @@ var ImageManager = function()
 				height: "" + backgroundImage.height,
 				width: "" + backgroundImage.width
 			})
-						
+
 			jQuery.each(images, function(i)
 			{
 				var positionIndex = 0;
 				for (positionIndex = 0; positionIndex < images[i].positions.length; positionIndex = positionIndex + 1)
-				{	
-					$('<img id="'+ images[i].id+'" src="' +	 images[i].src + 
+				{
+					$('<img id="'+ images[i].id+'" src="' +	 images[i].src +
 					'" style="position:absolute; top:' + images[i].positions[positionIndex].y+
-					'; left: '+images[i].positions[positionIndex].x+';" />').appendTo('#gameWrapper');								
-				} 
+					'; left: '+images[i].positions[positionIndex].x+';" />').appendTo('#gameWrapper');
+				}
 					//$('#' + images[i].id).animate
-					
+
 			})
 		})
 	}
-	
-	module.exports.ImageManager = function() {
-	return new ImageManager();
-}
-	
 }
